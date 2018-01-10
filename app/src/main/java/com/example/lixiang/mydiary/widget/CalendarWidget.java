26d1c5de840809b6b4999adcf048497b0a947751
Aroya <aroya@sina.com>
2018-01-10 12:28:58 +0800

    使用广播"android.appwidget.action.APPWIDGET_UPDATE" Extra:"Action"为Int类型值0 可以进行切换显示模式的操作

--- app/src/main/java/com/example/lixiang/mydiary/widget/CalendarWidget.java
+++ app/src/main/java/com/example/lixiang/mydiary/widget/CalendarWidget.java
@@ -1,18 +1,24 @@
 package com.example.lixiang.mydiary.widget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 
+import android.net.Uri;
+import android.view.View;
 import android.widget.RemoteViews;
 
 import com.example.lixiang.mydiary.R;
+import com.example.lixiang.mydiary.activity.MainActivity;
+import com.example.lixiang.mydiary.activity.SplashActivity;
 
 
 public class CalendarWidget extends AppWidgetProvider {
 
+    boolean displayNotes=true;
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager,
@@ -35,9 +41,19 @@ public class CalendarWidget extends AppWidgetProvider {
         //默认为空的View
         remoteViews.setEmptyView(R.id.calendarWidget, R.layout.none_data);
 
+        //点击唤醒app
+//        Intent toApp=new Intent(context, SplashActivity.class);
+//        toApp.setAction("android.intent.action.MAIN");
+//        toApp.setData(Uri.parse(toApp.toUri(Intent.URI_INTENT_SCHEME)));
+//        toApp.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds[0]);
+//        toApp.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//
+//        PendingIntent pendingIntent=PendingIntent.getActivity(
+//                context,0,toApp,0);
+//        remoteViews.setOnClickPendingIntent(R.id.calendarWidget,pendingIntent);
+
         //更新Widget
         appWidgetManager.updateAppWidget(thisWidget, remoteViews);
-
     }
     //创建Widget
     @Override
@@ -55,12 +71,30 @@ public class CalendarWidget extends AppWidgetProvider {
     @Override
     public void onReceive(Context context, Intent intent) {
         super.onReceive(context, intent);
-            final AppWidgetManager mgr = AppWidgetManager.getInstance(context);
-            final ComponentName cn = new ComponentName(context,CalendarWidget.class);
+        int action=intent.getIntExtra("Action",-1);
+        final AppWidgetManager mgr = AppWidgetManager.getInstance(context);
+        final ComponentName cn = new ComponentName(context,CalendarWidget.class);
+        switch (action){
+            case -1://默认刷新
 
-            //调用Serivce中Factory的onDataSetChanged()
-            mgr.notifyAppWidgetViewDataChanged(mgr.getAppWidgetIds(cn),
-                    R.id.calendarWidget);
+                //调用Serivce中Factory的onDataSetChanged()
+                mgr.notifyAppWidgetViewDataChanged(mgr.getAppWidgetIds(cn),
+                        R.id.calendarWidget);
+                break;
+            case 0://显示模式切换
+                RemoteViews remoteViews=new RemoteViews(context.getPackageName(),R.layout.calendar_widget);
+                if(displayNotes){
+                    displayNotes=false;
+                    remoteViews.setViewVisibility(R.id.calendarWidget, View.GONE);
+                }
+                else{
+                    displayNotes=true;
+                    remoteViews.setViewVisibility(R.id.calendarWidget,View.VISIBLE);
+                }
+                mgr.updateAppWidget(cn,remoteViews);
+
+        }
+
     }
 
 }
